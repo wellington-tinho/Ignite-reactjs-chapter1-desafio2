9a707da49ba711f2d7a7bf8b6d7c9fa6b0734e3d
Wellington R <sousa_wellington@outlook.com>
2021-04-12 19:29:22 -0300

    ⭐Feat: Sidebar

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,40 @@
-export function SideBar() {
-  // Complete aqui
+
+import { Button } from "./Button";
+
+import '../styles/sidebar.scss';
+import '../styles/button.scss';
+
+interface SidebarProps{
+  handleClickButton: Function;
+  genres: Array<GenreResponseProps>;
+  selectedGenreId: number;
 }
+
+interface GenreResponseProps {
+  id: number;
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  title: string;
+}
+
+export function SideBar(props: SideBarProps) {
+  const { genres, handleClickButton, selectedGenreId } = props;
+  return(
+      <nav className="sidebar">
+      <span>Watch<p>Me</p></span>
+
+      <div className="buttons-container">
+        {genres.map(genre => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => handleClickButton(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+
+    </nav>
+  )
+}
+  
